10b82fe9e03eeea1b32152963cc9edf8d06d72a9
Tibor <36704519+tibor3741@users.noreply.github.com>
2024-02-04 13:54:01 +0100

    Képtámogatást is beillesztettem a felhasználóbarát felületért
    Mostantól a Torrent Kódközpont alkalmazás képes megjeleníteni a kiválasztott streaming szolgáltatások ikonjait a GUI-n. Ezzel tovább növelve a felhasználói élményt és a könnyű azonosítást.
    Emellett beállítottam egy háttérképet is a Torrent Kódközpont alkalmazásnak, ami tovább javítja a felhasználói élményt és esztétikai megjelenést.

--- main.py
+++ main.py
@@ -1,154 +1,113 @@
 import tkinter as tk
 from tkinter import messagebox
 import pyperclip
+from PIL import Image, ImageTk
+import os
 
-class StreamingApp:
+class CodeCenterApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Torrent Kódközpont")
+        self.root.title("Kód Központ")
+        self.root.geometry("800x600")
+        self.root.configure(bg='#000000')  # Fekete háttér
 
-        self.data = {
-            "Netflix": ".nf.",
-            "Amazon Prime": ".amzn.",
-            "Hbo Max": ".hmax.",
-            "Disney Plus": ".dsnp.",
-            "Sky Show Time": ".skst.",
-            "HULU": ".hulu.",  
-            "Tv2": ".tv2.",
-            "RTL PLUSZ": ".rtlp."
-        }
-
+        # Adattag hozzáadása
+        self.setup_data()
         self.setup_ui()
 
+    def setup_data(self):
+        self.data = {
+            "Netflix": {"code": ".nf.", "icon": "netflix_logo.png"},
+            "Amazon Prime Video": {"code": ".amzn.", "icon": "amazon_prime_video_logo.png"},
+            "Disney Plus": {"code": ".dsnp.", "icon": "disney_logo.png"},
+            "Hbo Max": {"code": ".hmax.", "icon": "hbo_logo.png"},
+            "Sky Show Time": {"code":".skst.", "icon": "skyshowtime_logo.png"},
+            "Hulu": {"code": ".hulu.", "icon": "hulu_logo.png"},
+            "RTL Klub": {"code": ".rtlp.", "icon": "rtl_logo.png"},
+            "TV2 Play": {"code": ".tv2.", "icon": "tv2_play_logo.png"},
+        }
+
     def setup_ui(self):
-        # Fő cím
-        title_label = tk.Label(self.root, text="Streaming Szolgáltatások és Kódjaik", font=("Helvetica", 16))
-        title_label.grid(row=0, column=0, columnspan=2, pady=10)
+        # Fő keret
+        main_frame = tk.Frame(self.root, bg='#000000')  # Fekete háttér
+        main_frame.pack(fill=tk.BOTH, expand=True)
 
-        self.setup_listbox(1, 0)
-        self.setup_listbox(1, 1, width=10)
+        # Háttérkép beállítása
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        image_path = os.path.join(current_dir, "Images", "wallpaper.jpg")
+        background_image = Image.open(image_path)
+        background_photo = ImageTk.PhotoImage(background_image)
 
-        self.setup_scrollbars()
-        self.setup_exit_button()
+        # Háttérkép widget
+        background_label = tk.Label(main_frame, image=background_photo)
+        background_label.image = background_photo
+        background_label.place(relwidth=1, relheight=1)
 
-        self.disable_elements_without_dot(None)
+        # Cím
+        title_label = tk.Label(main_frame, text="Kód Központ", font=("Courier", 20, "bold"), fg='#00ff00', bg='#000000')  # Zöld szöveg, fekete háttér
+        title_label.pack(pady=20)
 
-        # Egy változó a jobb kattintás követéséhez
+        # Szolgáltatók gombjai tartalmazó Frame
+        buttons_frame = tk.Frame(main_frame, bg='#000000')
+        buttons_frame.pack()
+
+        # Gombok és kódok listaszerű elrendezése
+        row_count = 0
+        col_count = 0
+        for service, info in self.data.items():
+            icon_path = os.path.join("Images", info["icon"])
+            icon_image = Image.open(icon_path)
+            icon_photo = ImageTk.PhotoImage(icon_image)
+
+            button = tk.Button(buttons_frame, text=service, command=lambda s=service, c=info["code"]: self.update_selected_service(s, c), bg='#1a1a1a', fg='#00ff00', bd=1, relief=tk.FLAT, font=("Courier", 14), image=icon_photo, compound="top")
+            button.image = icon_photo
+            button.grid(row=row_count, column=col_count, pady=5, padx=10)
+
+            col_count += 1
+            if col_count == 4:
+                col_count = 0
+                row_count += 1
+
+        # Kód megjelenítő
+        self.code_label = tk.Label(main_frame, text="", font=("Courier", 14), fg='#00ff00', bg='#000000')  # Zöld szöveg, fekete háttér
+        self.code_label.pack(pady=50)
+
+        # Kilépés és Másolás gombok
+        quit_button = tk.Button(main_frame, text="Kilépés", command=self.root.destroy, bg='#ff0000', fg='#ffffff', bd=0, padx=10, pady=5, font=("Courier", 12, "bold"))
+        quit_button.place(relx=0.4, rely=0.95, anchor="s")
+
+        copy_button = tk.Button(main_frame, text="Másolás", command=self.copy_to_clipboard, bg='#337ab7', fg='#ffffff', bd=0, padx=10, pady=5, font=("Courier", 12, "bold"))
+        copy_button.place(relx=0.6, rely=0.95, anchor="s")
+        # Időzítő változók
         self.right_click_triggered = False
-
-        # Az időkorlát (60 másodperc) beállítása
         self.time_limit = 60000
-
-        # Az időzítő inicializálása
         self.timer = None
 
-        # Az események hozzárendelése
-        self.table2.bind("<Button-1>", self.left_click_handler)
-        self.table2.bind("<Button-3>", self.right_click_handler)
-
-        # Státusz sor
-        self.status_label = tk.Label(self.root, text="", bd=1, relief=tk.SUNKEN, anchor=tk.W)
-        self.status_label.grid(row=3, column=0, columnspan=2, sticky=tk.W + tk.E)
-
-    def setup_listbox(self, row, column, **kwargs):
-        listbox = tk.Listbox(
-            self.root,
-            selectmode="browse",
-            exportselection=0,
-            justify="center",
-            font=("Helvetica", 20),
-            selectbackground="black",
-            foreground="black",
-            bg="white",
-            **kwargs
-        )
-
-        listbox.grid(row=row, column=column, sticky="nsew", padx=5, pady=5)
-        listbox.bind("<Button-1>", self.handle_first_row_selection)
-
-        if column == 1:
-            listbox.bind("<Button-1>", self.disable_elements_without_dot)
-
-        setattr(self, f"table{column + 1}", listbox)
-        self.root.grid_columnconfigure(column, weight=1)
-
-        for item in self.data.keys():
-            getattr(self, f"table{column + 1}").insert(tk.END, item)
-
-    def setup_scrollbars(self):
-        scrollbar1 = tk.Scrollbar(self.root, command=self.table1.yview)
-        scrollbar1.grid(row=1, column=0, sticky='nse')
-        self.table1.configure(yscrollcommand=scrollbar1.set)
-
-        scrollbar2 = tk.Scrollbar(self.root, command=self.table2.yview)
-        scrollbar2.grid(row=1, column=1, sticky='nse')
-        self.table2.configure(yscrollcommand=scrollbar2.set)
-
-    def setup_exit_button(self):
-        quit_button = tk.Button(self.root, text="Kilépés", command=self.root.destroy)
-        quit_button.grid(row=2, column=0, columnspan=2, pady=10)
-
-    def handle_first_row_selection(self, event):
-        selected_item_index = self.table1.nearest(event.y)
-        selected_item = self.table1.get(selected_item_index)
-
-        if selected_item and selected_item in self.data:
-            self.table2.delete(0, tk.END)
-            self.table2.insert(tk.END, self.data[selected_item])
-            self.show_message("Sikeres kiválasztás", f"{selected_item} kódjai megjelenítve")
-
-        return "break"
-
-    def disable_elements_without_dot(self, event):
-        self.table2.selection_clear(0, tk.END)
-        self.table2.delete(0, tk.END)
+    def update_selected_service(self, service, code):
+        self.show_message(f"Sikeres kiválasztás - {service}", f"{service} kódjai megjelenítve")
+        self.code_label.config(text=code)
 
     def show_message(self, title, message):
-        self.status_label.config(text=message)
-        self.root.after(60000, self.clear_status)  # Törölje a státusz sort 60 másodperc után
-
-    def clear_status(self):
-        self.status_label.config(text="")  # Törölje a státusz sort
-
-    def left_click_handler(self, event):
-        # Bal kattintás esetén megszakítjuk az időzítőt
-        self.reset_timer()
-
-    def right_click_handler(self, event):
-        # Jobb kattintás esetén elindítjuk az időzítőt
-        self.start_timer()
-
-    def start_timer(self):
-        # Az időzítő indítása
-        self.timer = self.root.after(self.time_limit, self.timeout_handler)
-
-    def reset_timer(self):
-        # Az időzítő újraindítása
+        # Törlés a címkéről
+        self.clear_status()
+        # Megjelenítés a címkén
+        self.code_label.config(text=message)
+        # Időzített törlés
         if self.timer:
             self.root.after_cancel(self.timer)
             self.timer = None
+        self.timer = self.root.after(60000, self.clear_status)
 
-    def timeout_handler(self):
-        # Időkorlát lejártakor végrehajtandó műveletek
-        if not self.right_click_triggered:
-            self.show_message("Figyelem", "Nyomj jobb kattintást 60 másodpercig!")
-        else:
-            # Ha sikerült másolni a kódot, akkor üzenetet jelenítünk meg
-            self.show_message("Sikeres másolás", "Kód másolva a vágólapra!")
+    def clear_status(self):
+        self.code_label.config(text="")
 
     def copy_to_clipboard(self):
-        selected_items = [item for item in self.table2.get(0, tk.END) if item.startswith(".")]
-        if selected_items:
-            joined_items = " ".join(selected_items)
-            pyperclip.copy(joined_items)
-            self.show_message("Sikeres másolás", f"{joined_items} másolva a vágólapra")
+        selected_code = self.code_label.cget("text")
+        if selected_code:
+            pyperclip.copy(selected_code)
+            self.show_message("Sikeres másolás", f"{selected_code} másolva a vágólapra")
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = StreamingApp(root)
-
-    # Gomb a kód másolásához
-    copy_button = tk.Button(root, text="Kód másolása", command=app.copy_to_clipboard)
-    copy_button.grid(row=4, column=0, columnspan=2, pady=10)
-
+    app = CodeCenterApp(root)
     root.mainloop()
